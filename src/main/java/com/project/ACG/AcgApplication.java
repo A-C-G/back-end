0cff1b7f568827f3afc35541d42bfe0ec9a11ad4
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2023-09-26 19:36:12 +0900

    first commit

---
+++ src/main/java/com/project/ACG/AcgApplication.java
@@ -0,0 +1,15 @@
+package com.project.ACG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class AcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcgApplication.class, args);
+	}
+
+}
