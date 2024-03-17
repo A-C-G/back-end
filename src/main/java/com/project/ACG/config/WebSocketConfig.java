48977dbc3b2c8776ff6f139e8f9d81a9cbcb2eb0
HyunTae Jo <hyuntae9912@naver.com>
2024-03-17 16:29:59 +0900

    서버 이전

--- src/main/java/com/project/ACG/config/WebSocketConfig.java
+++ src/main/java/com/project/ACG/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("https://prod.hyunn.shop")
+    registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("https://prod.hyunn.site")
         .withSockJS();
     // 커넥션을 맺는 경로 설정. 만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
   }
